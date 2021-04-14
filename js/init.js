7cee6530e60961229812fdae1822bc4995f7e143
marh51m <82420981+marh51m@users.noreply.github.com>
2021-04-13 19:35:23 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
 firebase.initializeApp({
   apiKey: "AIzaSyAwc0TaPEKMi6gwnD-kmUk5eI_OyZ8ME5s",
     authDomain: "marh51m.firebaseapp.com",
+    databaseURL: "https://marh51m.firebaseio.com",
     projectId: "marh51m",
     storageBucket: "marh51m.appspot.com",
     messagingSenderId: "465311792906",
